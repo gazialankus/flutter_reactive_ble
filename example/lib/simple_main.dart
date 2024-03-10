130889ff2ffc04c2afdee04badd0bbbf10cf45fc
Gazihan Alankuş <gazialankus@gmail.com>
2024-03-10 03:49:41 +0300

    Organized into functions. Works.

--- example/lib/simple_main.dart
+++ example/lib/simple_main.dart
@@ -35,32 +35,38 @@ class MainPage extends StatelessWidget {
 
   Future<void> _run() async {
     final ble = FlutterReactiveBle();
+    print('RUN FlutterReactiveBle');
 
-    await ble.statusStream.firstWhere((e) => e == BleStatus.ready);
+    await waitUntilBleIsReady(ble);
+    print('RUN Ble is ready');
 
-    final idCompleter = Completer<String>();
-    final scanSubscription =
-        ble.scanForDevices(withServices: []).listen((event) {
-      if (event.id.toLowerCase().endsWith('7c:7e')) {
-        if (event.connectable == Connectable.available) {
-          idCompleter.complete(event.id);
-        }
-      }
-    });
+    final id = await scanAndGetId(ble);
+    print('RUN scanned and got ');
 
-    final id = await idCompleter.future;
-    await scanSubscription.cancel(); // TODO so this could be await for?
+    await waitUntilBonded(id);
+    print('RUN is bonded');
 
+    final connectionSubscription = await waitUntilConnected(ble, id);
+    print('RUN is connected');
 
-    final bondingState = await BleBonding().getBondingState(id);
+    // added later
+    await waitUntilBonded(id);
+    print('RUN is bonded 2');
 
-    if (bondingState != BleBondingState.bonded) {
-      print('was not bonded, is bonding');
-      await BleBonding().bond(id);
-      // works without this wait just fine
-      // await Future<void>.delayed(const Duration(seconds: 5));
-    }
+    await discoverAndPrintServices(ble, id);
+    print('Discovered services');
 
+    await connectionSubscription.cancel();
+  }
+
+  Future<void> discoverAndPrintServices(FlutterReactiveBle ble, String id) async {
+    await ble.discoverAllServices(id);
+    final services = await ble.getDiscoveredServices(id);
+    print('GOT SERVICES');
+    print(services);
+  }
+
+  Future<StreamSubscription<ConnectionStateUpdate>> waitUntilConnected(FlutterReactiveBle ble, String id) async {
     print('will connect');
     var isConnected = false;
 
@@ -88,13 +94,41 @@ class MainPage extends StatelessWidget {
       // this disconnects the device
       // await connectionSubscription.cancel();
     }
+    return connectionSubscription;
+  }
 
+  Future<void> waitUntilBonded(String id) async {
+    var bondingState = await BleBonding().getBondingState(id);
 
-    await ble.discoverAllServices(id);
-    final services = await ble.getDiscoveredServices(id);
-    print('GOT SERVICES');
-    print(services);
+    while (bondingState != BleBondingState.bonded) {
+      print('was not bonded, is bonding');
+      await BleBonding().bond(id);
 
-    await connectionSubscription.cancel();
+      bondingState = await BleBonding().getBondingState(id);
+      // works without this wait just fine
+      // await Future<void>.delayed(const Duration(seconds: 5));
+    }
+  }
+
+  Future<String> scanAndGetId(FlutterReactiveBle ble) async {
+    final idCompleter = Completer<String>();
+    final scanSubscription =
+        ble.scanForDevices(withServices: []).listen((event) {
+      if (event.id.toLowerCase().endsWith('7c:7e')) {
+        if (event.connectable == Connectable.available) {
+          idCompleter.complete(event.id);
+        }
+      }
+    });
+
+    final id = await idCompleter.future;
+    // so this could be await for? yes but would not work in real use.
+    // we're not looking for one device there.
+    await scanSubscription.cancel();
+    return id;
+  }
+
+  Future<void> waitUntilBleIsReady(FlutterReactiveBle ble) async {
+    await ble.statusStream.firstWhere((e) => e == BleStatus.ready);
   }
 }
