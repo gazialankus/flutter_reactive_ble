74b2f5cba68c7768ba780cecb01a3887ece2bf1c
Gazihan Alankuş <gazialankus@gmail.com>
2024-03-10 05:49:05 +0300

    Added the required Connectable.CONNECTABLE parameter to our fake scan results for connected devices.

--- packages/reactive_ble_mobile/android/src/main/kotlin/com/signify/hue/flutterreactiveble/ble/ReactiveBleClient.kt
+++ packages/reactive_ble_mobile/android/src/main/kotlin/com/signify/hue/flutterreactiveble/ble/ReactiveBleClient.kt
@@ -93,8 +93,9 @@ open class ReactiveBleClient(private val context: Context) : BleClient {
             .map { device ->
                 ScanInfo(device.macAddress, device.name ?: "",
                     0,
+                    Connectable.CONNECTABLE,
                     emptyMap(),
-                     emptyList(),
+                    emptyList(),
                     byteArrayOf(),
                 )
             }
